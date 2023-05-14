6632aa017a69c742319bc9aee6e1cbf133007d2b
Dmitry Gubich <gubich97@gmail.com>
2023-05-14 17:33:36 +0200

    14_05_2023

--- tasks/05_2023/12/compress_string.py
+++ tasks/05_2023/12/compress_string.py
@@ -1,38 +1,41 @@
 # Implement compress() and decompress() functions for a basic string compression scheme.
 # In particular, we would like to compress strings with long runs of the same character, for example "aaabbbbbbccdddd".
 
-
-def compress(s):
-    result = ""
-    counter = 1
-    for i, letter in enumerate(s):
-        try:
-            next_symbol = s[i + 1]
-        except IndexError:
-            result = update_result_string(counter, letter, result)
-        if next_symbol == letter:
-            counter += 1
-        if next_symbol != letter:
-            result = update_result_string(counter, letter, result)
-            counter = 1
-    return result
+from itertools import groupby
 
 
-def update_result_string(counter, letter, result):
-    if counter == 1:
-        result += letter
-    else:
-        result += f"{counter}{letter}"
-    return result
+def compress(data):
+    compressed_seq = ""
+    compressed = [(x, len(list(y))) for x, y in groupby(data)]
+    for _, elem in enumerate(compressed):
+        for j in elem:
+            if j != 1:
+                compressed_seq += str(j)
+    return compressed_seq
+    # result = ""
+    # counter = 1
+    # for i, letter in enumerate(s):
+    #     try:
+    #         next_symbol = s[i + 1]
+    #     except IndexError:
+    #         result = update_result_string(counter, letter, result)
+    #     if next_symbol == letter:
+    #         counter += 1
+    #     if next_symbol != letter:
+    #         result = update_result_string(counter, letter, result)
+    #         counter = 1
+    # return result
 
 
-def decompress(s):
-    pass
+# def update_result_string(counter, letter, result):
+#     if counter == 1:
+#         result += letter
+#     else:
+#         result += f"{counter}{letter}"
+#     return result
 
 
 if __name__ == "__main__":
     assert "abc" == compress("abc")
-    assert "3ag6b2c4df" == compress("aaagbbbbbbccddddf")
-    assert "3a6b2c3a4df" == compress("aaabbbbbbccaaaddddf")
-    # compressed_str = compress(init_str)
-    # assert init_str == decompress(compressed_str)
+    assert "a3gb6c2d4f" == compress("aaagbbbbbbccddddf")
+    assert "a3b6c2a3d4f" == compress("aaabbbbbbccaaaddddf")
