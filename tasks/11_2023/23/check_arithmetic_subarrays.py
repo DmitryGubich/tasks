444bdc496968127a3e568b8c338d02817c643718
Dmitry Gubich <gubich97@gmail.com>
2023-11-23 22:09:18 +0100

    23_11_2023

---
+++ tasks/11_2023/23/check_arithmetic_subarrays.py
@@ -0,0 +1,21 @@
+def check_arithmetic_subarrays(
+    nums: list[int], l: list[int], r: list[int]
+) -> list[bool]:
+    def check_arithmetic_array(sub: list[int]) -> bool:
+        sub.sort()
+        dif = sub[1] - sub[0]
+        for i in range(2, len(sub)):
+            if sub[i] - sub[i - 1] != dif:
+                return False
+        return True
+
+    res = []
+    for q in range(len(l)):
+        res.append(check_arithmetic_array(nums[l[q] : r[q] + 1]))
+    return res
+
+
+if __name__ == "__main__":
+    assert check_arithmetic_subarrays(
+        nums=[4, 6, 5, 9, 3, 7], l=[0, 0, 2], r=[2, 3, 5]
+    ) == [True, False, True]
