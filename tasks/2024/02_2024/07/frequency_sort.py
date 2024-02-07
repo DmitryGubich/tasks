3639a487d3b56ee6a5f9c3fae6577beae28ec389
Dmitry Gubich <gubich97@gmail.com>
2024-02-07 16:57:22 +0100

    07_02_2024

---
+++ tasks/2024/02_2024/07/frequency_sort.py
@@ -0,0 +1,18 @@
+def frequency_sort(s: str) -> str:
+    hashmap = {}
+    for i in s:
+        if hashmap.get(i):
+            hashmap[i] += 1
+        else:
+            hashmap[i] = 1
+
+    result = ""
+    for k in sorted(hashmap.keys(), key=lambda x: -hashmap[x]):
+        result += f"{k*hashmap[k]}"
+
+    return result
+
+
+if __name__ == "__main__":
+    assert frequency_sort(s="tree") == "eetr"
+    assert frequency_sort(s="cccaaa") == "cccaaa"
