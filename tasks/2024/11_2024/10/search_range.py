95196467d92bf788d88c91d5c6b979174565bbbf
Dmitry Gubich <gubich97@gmail.com>
2024-11-11 00:01:10 +0100

    11_11_2024

--- tasks/2024/11_2024/10/search_range.py
+++ tasks/2024/11_2024/10/search_range.py
@@ -1,23 +1,33 @@
 def search_range(nums: list[int], target: int) -> list[int]:
-    def search(elem: int) -> int:
-        low = 0
-        high = len(nums)
-
-        while low < high:
+    def find_first(nums, target):
+        low, high = 0, len(nums) - 1
+        first = -1
+        while low <= high:
             mid = (low + high) // 2
-            if nums[mid] < elem:
+            if nums[mid] == target:
+                first = mid
+                high = mid - 1
+            elif nums[mid] < target:
                 low = mid + 1
             else:
-                high = mid
-        return low
+                high = mid - 1
+        return first
 
-    lo = search(target)
-    hi = search(target + 1) - 1
+    def find_last(nums, target):
+        low, high = 0, len(nums) - 1
+        last = -1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                last = mid
+                low = mid + 1
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return last
 
-    if lo <= hi:
-        return [lo, hi]
-
-    return [-1, -1]
+    return [find_first(nums, target), find_last(nums, target)]
 
     # first, last = -1, -1
     # for i in range(len(nums)):
@@ -29,5 +39,7 @@ def search_range(nums: list[int], target: int) -> list[int]:
 
 
 if __name__ == "__main__":
+    assert search_range(nums=[2, 2], target=2) == [0, 1]
+    assert search_range(nums=[1], target=1) == [0, 0]
     assert search_range(nums=[5, 7, 7, 8, 8, 10], target=8) == [3, 4]
     assert search_range(nums=[5, 7, 7, 8, 8, 10], target=6) == [-1, -1]
