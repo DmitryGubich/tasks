4f9b4ea7ab36fa16d7bbc8cb97f7908af46a5551
Dmitry Gubich <gubich97@gmail.com>
2024-11-12 22:46:37 +0100

    12_11_2024

---
+++ tasks/2024/11_2024/12/find_max_length.py
@@ -0,0 +1,17 @@
+def find_max_length(nums: list[int]) -> int:
+    count_map = {0: -1}
+    result = 0
+    count = 0
+    for i, num in enumerate(nums):
+        count += 1 if num == 1 else -1
+        if count in count_map:
+            result = max(result, i - count_map[count])
+        else:
+            count_map[count] = i
+
+    return result
+
+
+if __name__ == "__main__":
+    assert find_max_length(nums=[0, 1]) == 2
+    assert find_max_length(nums=[0, 1, 0]) == 2
