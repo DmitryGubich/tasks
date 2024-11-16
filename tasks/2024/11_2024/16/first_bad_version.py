0f54e3fa341c08df40adc2802d29280dff43a07d
Dmitry Gubich <gubich97@gmail.com>
2024-11-16 22:06:14 +0100

    16_11_2024

---
+++ tasks/2024/11_2024/16/first_bad_version.py
@@ -0,0 +1,19 @@
+def isBadVersion(n: int):
+    ...
+
+
+def first_bad_version(n: int) -> int:
+    low = 1
+    high = n
+    while low < high:
+        mid = (low + high) // 2
+        if isBadVersion(mid):
+            high = mid
+        else:
+            low = mid + 1
+    return low
+
+
+if __name__ == "__main__":
+    assert first_bad_version(n=5) == 4
+    assert first_bad_version(n=1) == 1
